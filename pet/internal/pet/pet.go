c5bc8dae1f0a291355d4dd7c1360fe9285a1d178
Jack Butler <jack@jackhas.codes>
2019-10-15 23:56:08 +1000

    Pet model
    Adds models for pet and specific field types.

--- pet/internal/pet/pet.go
+++ pet/internal/pet/pet.go
@@ -1,3 +1,53 @@
 package pet
 
+import (
+	"encoding/json"
+	"errors"
+)
 
+type Pet struct {
+	Id int64 `json:"id,omitempty"`
+	Category Meta `json:"category,omitempty"`
+	Name string `json:"name"`
+	PhotoUrls []string `json:"photoUrls"`
+	Tags []Meta `json:"tags,omitempty"`
+	Status Status `json:"status,omitempty"`
+}
+
+// InitPet creates an instance of Pet. name and photoUrls are required fields for this type.
+func InitPet(name string, photoUrls []string) *Pet {
+	return &Pet{Name:name, PhotoUrls:photoUrls}
+}
+
+type Meta struct {
+	Id int64 `json:"id"`
+	Name string `json:"name"`
+}
+
+type Status int
+const (
+	INVALID Status = iota
+	AVAILABLE
+	PENDING
+	SOLD
+)
+func (s Status) String() string {
+	return [...]string{"","available","pending","sold"}[s]
+}
+func (s *Status) UnmarshalJSON(b []byte) error {
+	var j string
+	err := json.Unmarshal(b, &j)
+	if err != nil {
+		return err
+	}
+	strings := map[string]Status{
+		"available":AVAILABLE,
+		"pending":PENDING,
+		"sold":SOLD,
+	}
+	*s = strings[j]
+	if *s == INVALID {
+		return errors.New("invalid value for type Status")
+	}
+	return nil
+}
---
+++ pet/internal/pet/pet_test.go
@@ -0,0 +1,67 @@
+package pet
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestInitPet(t *testing.T) {
+	p := InitPet("test", []string{"http://test.com"})
+	if p.Name != "test" {
+		t.Errorf("pet instance did not populate name")
+	}
+	if len(p.PhotoUrls) != 1 {
+		t.Errorf("pet instance did not populate photoUrls")
+	}
+}
+
+func TestStatus_String(t *testing.T) {
+	a := AVAILABLE.String()
+	p := PENDING.String()
+	s := SOLD.String()
+
+	if a != "available" {
+		t.Errorf("Expected 'available', got %s ", a)
+	}
+	if p != "pending" {
+		t.Errorf("Expected 'pending', got %s ", p)
+	}
+	if s != "sold" {
+		t.Errorf("Expected 'sold', got %s ", s)
+	}
+}
+
+func TestStatus_UnmarshalJSON(t *testing.T) {
+	r := map[string]Status{}
+	d := []byte(`
+{
+	"soldstatus":"sold",
+	"availablestatus":"available",
+	"pendingstatus":"pending"
+}`)
+	err := json.Unmarshal(d, &r)
+	if err != nil {
+		t.Errorf("should not have errored on unmarshal: %v", err)
+	}
+	if r["soldstatus"] != SOLD {
+		t.Errorf("expected SOLD, got %v", r["soldstatus"])
+	}
+	if r["availablestatus"] != AVAILABLE {
+		t.Errorf("expected AVAILABLE, got %v", r["availablestatus"])
+	}
+	if r["pendingstatus"] != PENDING {
+		t.Errorf("expected PENDING, got %v", r["pendingstatus"])
+	}
+}
+
+func TestStatus_UnmarshalJSON_InvalidCase(t *testing.T) {
+	r := map[string]Status{}
+	d := []byte(`
+{
+	"status":"invalid"
+}`)
+	err := json.Unmarshal(d, &r)
+	if err == nil {
+		t.Errorf("this should have thrown an error, got %v", r)
+	}
+}
